b254d9d07c4884278450d2ed05e2f379c38bdb9e
ansh3108 <anshkumar9686@gmail.com>
2025-04-04 23:05:30 +0530

    some updates

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document,addEventListener("DOMContentLoaded", function() {
+    const darkModeToggle= document.getElementById("darkModeToggle")
+    if(darkModeToggle) {
+        darkModeToggle.addEventListener("click", function() {
+            document.body.classList.toggle("dark-mode");
+        
 
+        if(document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("dark-mode", "enabled");
+        } else {
+            localStorage.setItem("dark-mode", "disabled");
+        }
+        
+});
+    } else {
+        console.error("Darkmode button not found")
+    }
+})
